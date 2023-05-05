a4b2ca36c553160b18c7b58256cf5dbe34e6f2d4
Oseias Saboia <oseiassaboia2@gmail.com>
2023-05-05 14:43:39 -0300

    Final crypt

--- src/modules/users/useCases/create-user/create-user.controller.ts
+++ src/modules/users/useCases/create-user/create-user.controller.ts
@@ -2,16 +2,23 @@ import { Request, Response } from 'express'
 import { logger } from '../../../../utils/logger'
 import { CreateUserUseCase } from './create-user.usecase'
 import { IUserRepository } from '../../repositories/user.repository'
+import { IPasswordCrypto } from '../../../../infra/shared/crypto/password.crypto'
 
 export class CreateUserController {
-  constructor(private userRepository: IUserRepository) {}
+  constructor(
+    private userRepository: IUserRepository,
+    private passwordBcrypt: IPasswordCrypto
+  ) {}
 
   async handle(req: Request, res: Response) {
     logger.info('User begin created')
     try {
       const data = req.body
 
-      const useCase = new CreateUserUseCase(this.userRepository)
+      const useCase = new CreateUserUseCase(
+        this.userRepository,
+        this.passwordBcrypt
+      )
       const result = await useCase.execute(data)
 
       return res.json(result)
