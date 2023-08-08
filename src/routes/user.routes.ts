eaf5baba94532e140808ebe693b12768cf3cd15c
Oseias Saboia <oseiassaboia2@gmail.com>
2023-08-08 16:03:03 -0300

    route authenticate

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import { createUserController } from '../modules/users/useCases/create-user';
+import { authenticateUserController } from '../modules/users/useCases/authenticate-user';
 import { Router } from 'express'
 
 const userRouter = Router()
 
 userRouter.post("/users", async (req, res) => {
   await createUserController.handle(req, res)
+})
+
+userRouter.post("/login", async (req, res) => {
+  await authenticateUserController.handle(req, res)
 })
 
 export { userRouter } 
